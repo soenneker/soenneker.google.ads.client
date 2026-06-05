2b7e794b42a92882376f2090f712188e9aa5b7b4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:52 -0500

    xml commenting

--- src/Soenneker.Google.Ads.Client/Abstract/IGoogleAdsClientUtil.cs
+++ src/Soenneker.Google.Ads.Client/Abstract/IGoogleAdsClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Google.Ads.Client.Abstract;
 /// </summary>
 public interface IGoogleAdsClientUtil : IAsyncDisposable, IDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<GoogleAdsClient> Get(CancellationToken cancellationToken = default);
 }
